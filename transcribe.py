6b44fc27d04dbb14bc24f628345e7a6268c58750
pysolver33 <pysolver33@gmail.com>
2022-10-15 17:27:33 -0400

    bash script

--- transcribe.py
+++ transcribe.py
@@ -73,5 +73,7 @@ def main():
 if __name__ == '__main__':
     """
     export AAI_API_KEY=50e2ab13db6d43e39e8f2d0e9b5533c7
+    set AAI_API_KEY=50e2ab13db6d43e39e8f2d0e9b5533c7
+    python transcribe.py audio/003.wav --local
     """
     main()
