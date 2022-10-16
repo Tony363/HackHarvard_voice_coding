487ff1f02fa3602bf2eb84affaae3eca26dd915c
pysolver33 <pysolver33@gmail.com>
2022-10-16 03:47:17 -0400

    cleaning

--- gui/audio_to_text.py
+++ gui/audio_to_text.py
@@ -1,26 +1,30 @@
 from google.cloud import speech
 
+
 def giveText(file_name):
     client = speech.SpeechClient.from_service_account_file('key.json')
 
-    with open(file_name,'rb') as f:
+    with open(file_name, 'rb') as f:
         mp3_data = f.read()
 
     audio_file = speech.RecognitionAudio(content=mp3_data)
 
     config = speech.RecognitionConfig(
-        sample_rate_hertz = 8000,
-        enable_automatic_punctuation = True,
-        language_code = 'en-US'
+        sample_rate_hertz=8000,
+        enable_automatic_punctuation=True,
+        language_code='en-US'
     )
 
     response = client.recognize(
-        config = config,
-        audio =  audio_file
+        config=config,
+        audio=audio_file
     )
 
+    r = ""
     for result in response.results:
-        return result.alternatives
+        r += result.alternatives
+    return r
+
 
 if __name__ == "__main__":
-    print(giveText("output.wav"))
+    print(giveText("../test4.wav"))
